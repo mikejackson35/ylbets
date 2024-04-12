9f317a4df3a6e0fd7d251dec21fe105c795e5ded
Mike_Jackson <mikejackson35@gmail.com>
2024-04-12 11:56:45 -0700

    uddate

--- ylbets.py
+++ ylbets.py
@@ -16,16 +16,20 @@ st.set_page_config(
 #plotly configs
 config = {'displayModeBar': False}
 
-#altair configs
-alt.themes.enable("dark")
-
 # dg_key = st.secrets.dg_key
-title_placeholder = st.empty()
+ph1 = st.empty()
 
 market_type = st.selectbox(
     'Choose Market',
     ('win','top_5','top_10','top_20')
 )
+
+with st.expander('detail'):
+    ph2 = st.empty()
+    ph3 = st.empty()
+    ph4 = st.empty()
+    st.write("#")
+    ph5 = st.empty()
 
 def main():
     
@@ -45,7 +49,10 @@ def main():
 
     return styled_df
 
-with title_placeholder:
-    st.subheader('ylbets')
+ph1.header('ylbets')
+ph2.write("Odds = real odds per datagolf.com")
+ph3.write("Agg = average line being offered across all sportsbooks")
+ph4.write("EV = expected value (ie. expected net profit on a 1-unit bet placed many times)")
+ph5.write("Expected Value Example - Consider the example of betting on coin flips (which, really, is not so different from betting on golf). The probability of flipping Heads or Tails is equal to 50%. Suppose a bookmaker offers +100 American odds, which implies a probability of 1/2 or 50%. Given that the implied probability is equal to the true probability of Heads, the expected value from betting on Heads is zero. If a bookmaker offered odds of -110, the expected value would be negative (-5%, or -0.05 per unit bet); if a dumb bookmaker offered odds of +110, the expected value would then be positive (+5%), and in theory a bet worth taking.")
 
-st.dataframe(main(), hide_index=True, height=3000)#,use_container_width=True)#, column_config={'Agg Line':None})
+st.dataframe(main(), hide_index=True, height=3000 ,use_container_width=True)#, column_config={'Agg Line':None})
