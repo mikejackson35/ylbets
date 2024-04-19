81a672d10b9685aba4a5a78f4f7cb3fe88d8026d
Mike_Jackson <mikejackson35@gmail.com>
2024-04-19 00:28:03 -0700

    update

--- utils.py
+++ utils.py
@@ -112,7 +112,6 @@ def get_ev_table(market_type):
     df['market_type'] = market_type
     df['target_euro'] = df['market_type'].map(market_target_dict) * df['dec_odds']
     df['target_american'] = df['target_euro'].apply(convert_euro_to_american)
-    df['target_american'] = df['target_american'].astype(int)
 
     # add expected value column (for color)
     df['ev'] = (1 / df['dec_odds']) * df['ag_dec'] -1
@@ -122,6 +121,6 @@ def get_ev_table(market_type):
 
     # column names
     df = df[['player_name','target_american','ev','am_odds','ag_am']]
-    df = df.convert_dtypes().round(2)
+    df = df.convert_dtypes()
 
     return df
