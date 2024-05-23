2362fcb42059a4e39c62a8bb3b3a144725191231
Mike_Jackson <mikejackson35@gmail.com>
2024-05-23 14:04:20 -0700

    udpate

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def get_our_plays_table(our_plays):
     try:
         # read in live odds
         df = pd.read_csv(LIVE_ODDS).convert_dtypes()
-        df = df[['player_name', 'current_score', 'current_pos', 'top_20', 'top_10', 'top_5', 'win']]
+        df = df[['player_name', 'current_score', 'current_pos', 'top_10', 'top_5', 'win']]
 
         # format percentages
         df['win'] = ((df['win'] * 100).round()).astype(int)#.astype(str) + '%'
@@ -77,7 +77,7 @@ def get_our_plays_table(our_plays):
         # filter to selected plays and needed columns
         our_plays_table = df[df['player_name'].isin(our_plays)].round(2).reset_index(drop=True)
         our_plays_table['player_name'] = fix_names(our_plays_table['player_name'])
-        our_plays_table.columns = ['Player', 'Tot', 'Pos','% T20','% T10','% T5','% Win']
+        our_plays_table.columns = ['Player', 'Tot', 'Pos','% T10','% T5','% Win']
 
         return our_plays_table
 
