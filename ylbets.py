10e550cb18a43b2ce2b18173719b06a78d504583
Mike_Jackson <mikejackson35@gmail.com>
2024-05-23 14:40:40 -0700

    update

--- ylbets.py
+++ ylbets.py
@@ -88,19 +88,12 @@ our_plays = [
 # st.markdown(" ")
 st.markdown(f"<h4>Live Plays</h4>", unsafe_allow_html=True)
 
-st.markdown("""
-McCarthy  
-                
-    win +5000   T5 +1000      T10 +450
-
-Horschel
-    
-    T5 +1100    T10 +530
-
-Poston
-            
-    T5  +1400   T10 +600
-            """,unsafe_allow_html=True)
+st.write("McCarthy<br><small>win +5000<br> T5 +1000<br> T10 +450",unsafe_allow_html=True)
+st.write(" ")
+st.write("Horschel<br><small>T5 +1100<br>T10 +530",unsafe_allow_html=True)
+st.write(" ")
+st.write("Poston<br><small>T5  +1400<br>T10 +600",unsafe_allow_html=True)
+st.write(" ")
 
 st.caption(f"last update: {get_update_stamp(LIVE_ODDS)['last_update'][0]}", unsafe_allow_html=True)
 our_plays_table = get_our_plays_table(our_plays)
